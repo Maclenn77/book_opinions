9f0c1d234d11559683e710b80ad342795daec0f1
Maclenn77 <maclenn77@gmail.com>
2020-07-28 12:54:35 -0500

    Create methods that return total followers and followees

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :opinions
   has_many :followed_users, foreign_key: :follower_id, class_name: 'Following'
   has_many :followees, through: :followed_users
+
+  def total_followed_users
+    followed_users.count
+  end
+
+  def total_followees
+    followees.count
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'the total followed users' do
+    assert_equal(5,total_followed_users)
+  end
+  
 end
