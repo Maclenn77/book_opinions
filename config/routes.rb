3e161b58d09f4a576cca7bd9eba3acc383ddd16a
Maclenn77 <maclenn77@gmail.com>
2020-07-29 16:27:45 -0500

    Add partials of header"

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  delete 'signout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
