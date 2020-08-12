cee8195198bb52cc01359281e347590a4c65d699
Maclenn77 <maclenn77@gmail.com>
2020-08-12 17:59:10 -0500

    Add button to follow users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,12 @@
 module ApplicationHelper
   def follow(user, current_user)
-
     following = user.followed?(current_user)
 
     if following
-      link_to('Unfollow', following_path(id: following.id), method: :delete)
+      link_to('add_circle', following_path(id: following.id), method: :delete)
     else
-      link_to('Follow', followings_path(user_id: current_user.id,
-                                        followee_id: user.id), method: :post)
+      link_to('add_circle_outline', followings_path(user_id: current_user.id,
+                                                    followee_id: user.id), method: :post)
     end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -18,4 +18,20 @@ module UsersHelper
     c.total_followees unless current_user.nil?
   end
 
+  def avatar(user)
+    if user.avatar.attached?
+      image_tag user.avatar.variant(combine_options: {resize: '60x60^', extent: '60x60', gravity: 'Center'} ), class: 'avatar m-2 rounded float-left'
+    else
+      image_tag('avatar.png', alt: user.name, width: 60, height: 60, class: 'avatar m-2 rounded float-left')
+    end
+  end
+
+  def circle_avatar(user)
+    if user.avatar.attached?
+      image_tag user.avatar.variant(combine_options: {resize: '60x60^', extent: '60x60', gravity: 'Center'} ), class: 'avatar m-2 rounded-circle float-left'
+    else
+      image_tag('avatar.png', alt: user.name, width: 60, height: 60, class: 'avatar m-2 rounded-circle float-left')
+    end
+  end
+
 end
