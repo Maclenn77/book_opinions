1d85f25ab84f9bd11703184c832157ad6ebd991b
Maclenn77 <maclenn77@gmail.com>
2020-09-11 17:15:33 -0500

    Create features specs for opinions with rspec

--- spec/features/opinion_spec.rb
+++ spec/features/opinion_spec.rb
@@ -1,38 +1,36 @@
-# require 'test_helper'
-#
-# class OpinionFlowTest < ActionDispatch::IntegrationTest
-#   self.use_instantiated_fixtures = true
-#
-#   setup do
-#     Capybara.current_driver = Capybara.javascript_driver # :selenium by default
-#   end
-#
-#   test 'create an new opinion' do
-#     user = users(:juan)
-#
-#     visit login_path
-#
-#     fill_in('session_name', with: user.name)
-#
-#     click_button('Log in')
-#
-#     sleep(3)
-#
-#     assert page.has_content?('Share your book opinions')
-#
-#     fill_in('opinion_title', with: 'Test for Dummies')
-#     fill_in('opinion_body', with: 'It\'s a dummy book')
-#
-#     click_button('Post')
-#
-#     sleep(3)
-#
-#     assert page.has_content?('It\'s a dummy book')
-#
-#     find_link('Test for Dummies').visible?
-#
-#     visit root_path
-#
-#     accept_confirm { click_link('Log out') }
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe 'followings', type: :feature do
+  fixtures :users, :opinions, :followings
+  let(:user) { users(:juan) }
+
+  setup do
+    Capybara.current_driver = Capybara.javascript_driver # :selenium by default
+  end
+
+  scenario 'create an new opinion' do
+    visit login_path
+
+    fill_in('session_name', with: user.name)
+
+    click_button('Log in')
+
+    sleep(3)
+
+    expect(page).to have_content(/(Share your book opinions)/)
+    fill_in('opinion_title', with: 'Test for Dummies')
+    fill_in('opinion_body', with: 'It\'s a dummy book')
+
+    click_button('Post')
+
+    sleep(3)
+
+    expect(page).to have_content(/(a dummy book)/)
+
+    find_link('Test for Dummies').visible?
+
+    visit root_path
+
+    accept_confirm { click_link('Log out') }
+  end
+end
